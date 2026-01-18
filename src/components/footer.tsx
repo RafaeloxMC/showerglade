37e719cc552e7da00716fc4dc9c4917a7290de72
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-01-18 20:30:29 +0100

    Change footer styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 function Footer() {
 	return (
-		<div>
-			<div className="fixed bottom-0 w-full">
+		<div className="absolute w-screen -z-10 bottom-0">
+			<div className="fixed bottom-0 w-full h-64">
 				<Image
 					alt="footer background"
 					src="/footer.png"
 					width={1440}
 					height={256}
+					className="aspect-1140/256 object-cover w-screen bottom-0"
 				/>
 				<p className="absolute bottom-16 text-center w-screen text-[#123b49]">
 					&copy; 2026 - Made by @xvcf & @Lopa
